a9248f91aa99d69ae79d80b077ea961231f8db2e
Cedric David <cedric.david@btopenworld.com>
2018-09-09 02:02:35 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,9 +11,9 @@ class Scraper
       students.push(:name=>student.css(".student-name").text,
       :location=>student.css(".student-location").text,
       :profile_url=>student.css('a').attribute("href").value)
-    end
+    
     students
-
+  end
 
 
   def self.scrape_profile_page(profile_url)
