bec22c25c36171c1b10d61ea2b62349597e464dd
Cedric David <cedric.david@btopenworld.com>
2018-09-09 01:32:12 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,11 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     Nokogiri::HTML(open("index_url"))
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
