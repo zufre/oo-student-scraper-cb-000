dea9852ab41360fea8a1d551041dcd931f7fad48
Cedric David <cedric.david@btopenworld.com>
2018-09-09 01:52:29 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    Nokogiri::HTML(open("index_url"))
-    binding.pry
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css(".student-card").each do |student|
+      students.push(name: student.css(".student-name").text, 
+      location: student.css(".student-location").text, 
+      profile_picture: student.css('a').attribute("href").value)
   end
 
   def self.scrape_profile_page(profile_url)
@@ -14,4 +18,4 @@ class Scraper
   end
 
 end
-Scraper.new.scrape_index_page("./fixtures/student-site/index.html")
+
